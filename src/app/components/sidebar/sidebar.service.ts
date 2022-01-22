c7b6c55e9bee88b0e72ea3f76bed39e33b4eb2dd
Hugo Fernando <huginho888@outlook.com>
2022-01-22 20:53:11 -0300

    finalizando menu

--- src/app/components/sidebar/sidebar.service.ts
+++ src/app/components/sidebar/sidebar.service.ts
@@ -9,19 +9,23 @@ export class SidebarService implements ISidebarService {
   constructor() { }
 
   showHideMenu(sidebar: HTMLDivElement): void {
+    const main = document.querySelectorAll('main')[0]
     addEventListener('resize',()=>{
       if(document.body.clientWidth > 920){
           if(sidebar.classList.contains('left')){
               sidebar.classList.remove('left');
+              main.classList.remove('main-left')
           }
       }
-  })
+    })
 
     if(sidebar.classList.contains('left')){
       sidebar.classList.remove('left');
+      main.classList.remove('main-left')
 
-  } else {
-      sidebar.classList.add('left')
-  }
+    } else {
+        main.classList.add('main-left')
+        sidebar.classList.add('left')
+    }
   }
 }
