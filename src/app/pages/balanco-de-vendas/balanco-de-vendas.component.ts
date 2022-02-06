7717240dae128099438271a8a81ab7dd690ac8e8
Hugo Fernando <huginho888@outlook.com>
2022-02-05 23:18:24 -0300

    adicionando validação

--- src/app/pages/balanco-de-vendas/balanco-de-vendas.component.ts
+++ src/app/pages/balanco-de-vendas/balanco-de-vendas.component.ts
@@ -46,7 +46,9 @@ export class BalancoDeVendasComponent implements OnInit {
         this.porcentVendas = this.BalancoDeVendasService.porcentVendas
         this.porcentProdutoComMaisVendas = this.BalancoDeVendasService.porcentProdutoComMaisVendas
         this.produtoComMaisVendas = this.BalancoDeVendasService.produtoComMaisVendas.nome
-
+        if(!this.porcentProdutoComMaisVendas){
+          this.produtoComMaisVendas = 'Indefinido'
+        }
         if(this.porcentVendas >= 0){
           this.deltaPorcentVendas = 'maior'
         } else {
